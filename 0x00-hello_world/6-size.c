7f57e7a359263be5dc6f30aeb1fa8bb43f52ec4e
Medghar <gharbou9@gmail.com>
2023-03-30 03:33:00 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - main fonction
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	printf("Size of float: %lu byte(s)", sizeof(float);
+	printf("Size of char: %lu byte(s)", sizeof(char));
+	printf("Size of int: %lu byte(s)", sizeof(int));
+	printf("Size of long int: %lu byte(s)", sizeof(long int);
+	printf("Size of long long int: %lu byte(s)", sizeof(long long int);
+	return (0);
+}
